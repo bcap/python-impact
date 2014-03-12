57797c0dae526f34bdb4c3f0aaf36e8e413571f6
Bruno Polaco <polaco@gmail.com>
2014-03-12 15:24:29 -0300

    Separating settings from code, sending all the settings to the settings.py file

--- server.py
+++ server.py
@@ -7,21 +7,8 @@ import os
 import os.path
 import urlparse
 
-# Various config settings for the python server
-SETTINGS = {
-    'port':        8080,
-    'logging':     False,
-
-    'api-save':    '/lib/weltmeister/api/save.php',
-    'api-browse':  '/lib/weltmeister/api/browse.php',
-    'api-glob':    '/lib/weltmeister/api/glob.php',
-
-    'image-types': ['.png', '.jpg', '.gif', '.jpeg'],
-
-    'mimetypes': {
-        'ogg': 'audio/ogg'
-    }
-}
+# Settings comes from the settings.py file
+from settings import SETTINGS
 
 # Override port if we are on a Heroku server
 if os.environ.get('PORT'):
---
+++ settings.py
@@ -0,0 +1,15 @@
+# Various config settings for the python server
+SETTINGS = {
+    'port':        8080,
+    'logging':     False,
+
+    'api-save':    '/lib/weltmeister/api/save.php',
+    'api-browse':  '/lib/weltmeister/api/browse.php',
+    'api-glob':    '/lib/weltmeister/api/glob.php',
+
+    'image-types': ['.png', '.jpg', '.gif', '.jpeg'],
+
+    'mimetypes': {
+        'ogg': 'audio/ogg'
+    }
+}
